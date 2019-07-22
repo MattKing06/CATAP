d4dd4f5402ac5c98a9f4f2bcf30e069056f44308
Matt King <matthew.king@stfc.ac.uk>
2019-07-22 15:05:14 +0100

    printing out separator for LINUX build

--- ConfigReader/source/ConfigReader.cpp
+++ ConfigReader/source/ConfigReader.cpp
@@ -79,12 +79,11 @@ std::map<std::string, std::string> ConfigReader::parseYamlFile()
 	std::ifstream fileInput;
 	YAML::Node config;
 	YAML::Node configTemplate;
-	std::string separator;
 	#if defined(_unix_) || defined(_unix)
-		separator = "/";
+		const std::string separator = "/";
 	#endif
     #ifdef _WIN32
-	  separator = "\\";
+	  const std::string separator = "\\";
 	#endif
 	// before we start, config reader is usually a member to the Hardware classes
 	// and only gets initialized once, we need to reset PVs when parsing
