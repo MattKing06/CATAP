4cf2c94871d9a6595266ddd8946d51c05af80e69
Matt King <matthew.king@stfc.ac.uk>
2019-07-22 14:52:49 +0100

    corrected to access hardwareFolder not hardwareComponentName

--- ConfigReader/source/ConfigReader.cpp
+++ ConfigReader/source/ConfigReader.cpp
@@ -28,7 +28,8 @@ ConfigReader::ConfigReader(const std::string hardwareComponentName, bool isVirtu
 	#if defined(__unix__) ||  defined(_unix)
 	  const std::string HOME =  getenv("HOME");
 	  const std::string MASTER_LATTICE_FILE_LOCATION = HOME +"/MasterLattice";
-	  yamlFileDestination = MASTER_LATTICE_FILE_LOCATION + "/" + hardwareComponentName;
+	  hardwareFolder = getHardwareTypeFromName(hardwareComponentName);
+	  yamlFileDestination = MASTER_LATTICE_FILE_LOCATION + "/" + hardwareFolder;
 	#endif
 	#ifdef _WIN32
 	  yamlFilename = hardwareComponentName + ".yaml";
