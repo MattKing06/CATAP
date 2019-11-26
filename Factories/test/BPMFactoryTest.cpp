65bfd059d8ffe4571ad85426b3dd64c384c18fa6
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2019-11-26 13:48:24 +0000

    [bpm] adding tests

--- Factories/test/BPMFactoryTest.cpp
+++ Factories/test/BPMFactoryTest.cpp
@@ -10,6 +10,50 @@
 
 BOOST_AUTO_TEST_SUITE(BPMFactoryTestSuite)
 
+BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_sa1_test)
+{
+	std::string testBPMName = "VM-CLA-S02-DIA-BPM-01";
+	BPMFactory bpmfac = BPMFactory(true);
+	bpmfac.setup("nominal");
+	srand(time(NULL));
+	double sa1ToSet = rand() % 10;
+	bpmfac.setSA1(testBPMName, sa1ToSet);
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	BOOST_CHECK_EQUAL(bpmfac.getSA1(testBPMName), sa1ToSet);
+}
+
+//BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_data_test)
+//{
+//	BPM bpm = BPM();
+//	srand(time(NULL));
+//	std::vector< double > dataToSet(9);
+//	for (auto& it : dataToSet)
+//	{
+//		it = rand() % 10 + 1.0;
+//	}
+//	bpm.setData(dataToSet);
+//	BOOST_CHECK_EQUAL_COLLECTIONS(bpm.getData().begin(), bpm.getData().end(), dataToSet.begin(), dataToSet.end());
+//}
+//
+//BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_data_vector_test)
+//{
+//	BPM bpm = BPM();
+//	srand(time(NULL));
+//	std::vector< std::vector< double > > dataToSet(2);
+//	bpm.setVectorSize(2);
+//	for (auto&& it : dataToSet)
+//	{
+//		it.resize(9);
+//		for (auto&& it2 : it)
+//		{
+//			it2 = rand() % 10 + 1.0;
+//		}
+//	}
+//	bpm.setData(dataToSet.back());
+//	std::vector< double > get = bpm.getData();
+//	std::vector< double > set = dataToSet.back();
+//	BOOST_CHECK_EQUAL_COLLECTIONS(get.begin(), get.end(), set.begin(), set.end());
+//}
 
 //BOOST_AUTO_TEST_CASE(magnet_factory_read_i_magnet_test)
 //{
--- Hardware/test/BPMTest.cpp
+++ Hardware/test/BPMTest.cpp
@@ -4,7 +4,11 @@
 #include <boost/test/parameterized_test.hpp>
 #include <boost/bind.hpp>
 #include <string>
+#include <random>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <functional>
 #include "BPM.h"
 #include <vector>
 
@@ -18,11 +22,46 @@ BOOST_AUTO_TEST_CASE(creating_empty_bpm_test)
 	BOOST_CHECK_EQUAL(pvStructMap.empty(), true);
 }
 
-//BOOST_AUTO_TEST_CASE(setting_empty_magnet_psu_state_test)
-//{
-//	Magnet mag = Magnet();
-//	mag.setPSUState(STATE::OFF);
-//	BOOST_CHECK_EQUAL(mag.getPSUState(), STATE::OFF);
-//}
+BOOST_AUTO_TEST_CASE(bpm_set_and_check_x_test)
+{
+	BPM bpm = BPM();
+	srand(time(NULL));
+	double xToSet = rand() % 10 + 1.0;
+	bpm.setXPV(xToSet);
+	BOOST_CHECK_EQUAL(bpm.getXFromPV(), xToSet);
+}
+
+BOOST_AUTO_TEST_CASE(bpm_set_and_check_data_test)
+{
+	BPM bpm = BPM();
+	srand(time(NULL));
+	std::vector< double > dataToSet(9);
+	for(auto& it : dataToSet)
+	{
+		it = rand() % 10 + 1.0;
+	}
+	bpm.setData(dataToSet);
+	BOOST_CHECK_EQUAL_COLLECTIONS(bpm.getData().begin(), bpm.getData().end(), dataToSet.begin(), dataToSet.end());
+}
+
+BOOST_AUTO_TEST_CASE(bpm_set_and_check_data_vector_test)
+{
+	BPM bpm = BPM();
+	srand(time(NULL));
+	std::vector< std::vector< double > > dataToSet(2);
+	bpm.setVectorSize(2);
+	for (auto&& it : dataToSet)
+	{
+		it.resize(9);
+		for (auto&& it2 : it)
+		{
+			it2 = rand() % 10 + 1.0;
+		}
+	}
+	bpm.setData(dataToSet.back());
+	std::vector< double > get = bpm.getData();
+	std::vector< double > set = dataToSet.back();
+	BOOST_CHECK_EQUAL_COLLECTIONS(get.begin(), get.end(), set.begin(), set.end());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
