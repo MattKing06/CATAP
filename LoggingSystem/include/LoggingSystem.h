b70a1bbf7372c12f30d7d947cf06a4ec6a6eaf83
Matt King <matthew.king@stfc.ac.uk>
2019-11-27 18:13:08 +0000

    now using fprintf with stdout to fix the problem with async print calls happening from EPICS callback functions. Colour text removed as it did not work correctly

--- LoggingSystem/include/LoggingSystem.h
+++ LoggingSystem/include/LoggingSystem.h
@@ -4,150 +4,60 @@
 #include <iostream>
 #include <sstream>
 #include <initializer_list>
-#ifdef _WIN32
-// Windows.h defines macros min and max so numeric_limits::max fails.
-// defining NOMINMAX removes the min max macros from Windows.h
-#define NOMINMAX
-#include <Windows.h>
-#define FOREGROUND_WHITE (FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN)
-#endif
+
+#define DEBUG "[DEBUG]"
+#define MESSAGE "[MESSAGE]"
+
 class LoggingSystem
 {
 public:
-    LoggingSystem(){debugOn=false;messageOn=false;}
-    LoggingSystem(bool debugState, bool messageState);
-    void debugMessagesOn();
-    void debugMessagesOff();
-    void messagesOn();
-    void messagesOff();
-    bool isMessagingOn() const;
-    bool isDebugOn() const;
+	LoggingSystem() { debugOn = false; messageOn = false; }
+	LoggingSystem(bool debugState, bool messageState);
+	void debugMessagesOn();
+	void debugMessagesOff();
+	void messagesOn();
+	void messagesOff();
+	bool isMessagingOn() const;
+	bool isDebugOn() const;
 	std::string getCurrentDateAndTimeString() const;
-	bool multiString = false;
 
-#ifdef _WIN32
 	template<typename T>
-	void printMessage_oss(std::ostream& os, T t)
+	void generateStringStream(std::ostream& os, T t)
 	{
-		//single-value means no end-line char here..
-		if (multiString)
-		{
-			os << t;
-		}
-		else
-		{
-			os << t << std::endl;
-		}
+		os << t;
 	}
 	template<typename T, typename... Args>
-	void printMessage_oss(std::ostream& os, T t, Args... args)
+	void generateStringStream(std::ostream& os, T t, Args... args)
 	{
-		printMessage_oss(os, t);
-		printMessage_oss(os, args...);
+		generateStringStream(os, t);
+		generateStringStream(os, args...);
 	}
+
 	template<typename... Args>
 	void printMessage(Args... args)
 	{
 		if (messageOn)
 		{
-			multiString = true;
 			std::ostringstream oss;
-			printMessage_oss(oss, args...);
-			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			SetConsoleTextAttribute(hConsole, FOREGROUND_WHITE | FOREGROUND_INTENSITY);
-			std::cout << oss.str() << std::endl;
-			std::cout.flush();
-			multiString = false;
-		}
-	}
-#elif defined(_unix) || defined(__unix__)
-	void printMessage(T t)
-	{
-		if (messageOn)
-		{
-			std::cout << t << std::endl;
-		}
-	}
-	template<typename T, typename... Args>
-	void printMessage(T t, Args... args) // recursive variadic function
-	{
-		if (messageOn)
-		{
-			std::cout << t;
-			printMessage(args...);
+			generateStringStream(oss, args...);
+			std::ios::sync_with_stdio(true);
+			fprintf(stdout, "%s %s %s \n", getCurrentDateAndTimeString().c_str(), MESSAGE, oss.str().c_str());
 		}
 	}
 
-#endif
-
-
-#ifdef _WIN32
-	template<typename T>
-	void printDebugMessage_oss(std::ostream& os, T t)
-	{
-		//single-value means no end-line char here..
-		if (multiString)
-		{
-			os << t;
-		}
-		else
-		{
-			os << t << std::endl;
-		}
-	}
-	template<typename T, typename... Args>
-	void printDebugMessage_oss(std::ostream& os, T t, Args... args)
-	{
-		printDebugMessage_oss(os, t);
-		printDebugMessage_oss(os, args...);
-	}
 	template<typename... Args>
 	void printDebugMessage(Args... args)
 	{
-		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-		SetConsoleTextAttribute(hConsole, FOREGROUND_WHITE | FOREGROUND_INTENSITY);
 		if (debugOn)
 		{
-			std::stringstream oss;
-			multiString = true;
-			printDebugMessage_oss(oss, args...);
-			hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-			std::cout << oss.str() << std::endl;
-			SetConsoleTextAttribute(hConsole, FOREGROUND_WHITE | FOREGROUND_INTENSITY);
-			std::cout.flush();
-			multiString = false;
+			std::ostringstream oss;
+			generateStringStream(oss, args...);
+			std::ios::sync_with_stdio(true);
+			fprintf(stdout, "%s %s %s\n", getCurrentDateAndTimeString().c_str(), DEBUG, oss.str().c_str());
 		}
 	}
-#elif defined(_unix) || defined(__unix__)
-	template <typename T>
-	void printDebugMessage(T t)
-	{
-		if (debugOn)
-		{
-			std::cout << t << std::endl;
-	}
-	template<typename T, typename... Args>
-	void printDebugMessage(T t, Args... args)
-	{
-		if (debugOn)
-		{
-			std::cout << t;
-			printDebugMessage(args...);
-		}
-	}
-#endif
-
 private:
-    bool debugOn;
-    bool messageOn;
-#ifdef _WIN32
-	WORD GetConsoleTextAttribute(HANDLE hCon)
-	{
-		CONSOLE_SCREEN_BUFFER_INFO console_info;
-		GetConsoleScreenBufferInfo(hCon, &console_info);
-		return console_info.wAttributes;
-	}
-#endif
+	bool debugOn;
+	bool messageOn;
 };
 #endif // LOGGING_SYSTEM_H_
