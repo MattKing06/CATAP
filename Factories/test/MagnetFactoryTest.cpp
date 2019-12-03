27280c5981f684bc18c29742e3b8ddebe121873d
Matt King <matthew.king@stfc.ac.uk>
2019-12-03 11:38:38 +0000

    fixing tests to take status of setup into consideration before performing EPICS based tests

--- Factories/test/MagnetFactoryTest.cpp
+++ Factories/test/MagnetFactoryTest.cpp
@@ -14,16 +14,13 @@ BOOST_AUTO_TEST_CASE(magnet_factory_turn_on_magnet_test)
 {
 	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
 	MagnetFactory magFac = MagnetFactory(true);
+	magFac.messagesOn();
 	bool status = magFac.setup("nominal");
 	if (status)
 	{
 		magFac.turnOn(testMagnetName);
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 		BOOST_CHECK_EQUAL(magFac.getPSUState(testMagnetName), 1);
-	}
-	else
-	{
-		testLogger.printMessage("CANNOT CONNECT TO EPICS");
 	}
 
 }
@@ -32,6 +29,7 @@ BOOST_AUTO_TEST_CASE(magnet_factory_read_i_magnet_test)
 {
 	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
 	MagnetFactory magFac = MagnetFactory(true);
+	magFac.messagesOn();
 	bool status = magFac.setup("nominal");
 	if (status)
 	{
@@ -41,10 +39,6 @@ BOOST_AUTO_TEST_CASE(magnet_factory_read_i_magnet_test)
 		std::this_thread::sleep_for(std::chrono::seconds(10));
 		BOOST_CHECK_EQUAL(magFac.getRICurrent(testMagnetName), currentToSet);
 	}
-	else
-	{
-		testLogger.printMessage("CANNOT CONNECT TO EPICS");
-	}
 
 }
 
@@ -52,14 +46,11 @@ BOOST_AUTO_TEST_CASE(magnet_factory_rilk_state_test)
 {
 	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
 	MagnetFactory magFac = MagnetFactory(true);
+	magFac.messagesOn();
 	bool status = magFac.setup("nominal");
 	if (status)
 	{
 		BOOST_CHECK_EQUAL(magFac.getILKState(testMagnetName), 0);
-	}
-	else
-	{
-		testLogger.printMessage("CANNOT CONNECT TO EPICS");
 	}
 
 }
@@ -68,15 +59,12 @@ BOOST_AUTO_TEST_CASE(magnet_factory_get_all_magnet_currents_test)
 {
 	MagnetFactory magFac = MagnetFactory(true);
 	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
+	magFac.messagesOn();
 	bool status = magFac.setup("nominal");
 	if (status)
 	{
 		std::map<std::string, double> allMagCurrents = magFac.getAllMagnetCurrents();
 		BOOST_CHECK_NE(allMagCurrents.at(testMagnetName), std::numeric_limits<double>::min());
-	}
-	else
-	{
-		testLogger.printMessage("CANNOT CONNECT TO EPICS");
 	}
 
 }
@@ -88,34 +76,46 @@ BOOST_AUTO_TEST_CASE(magnet_factory_logging_system_test)
 	magFac.messagesOn();
 	BOOST_CHECK_EQUAL(magFac.isMessagingOn(), true);
 	BOOST_CHECK_EQUAL(magFac.reader.isMessagingOn(), true);
-	for (auto& mag : magFac.getAllMagnets())
+	if (status)
 	{
-		BOOST_CHECK_EQUAL(mag.second.isMessagingOn(), true);
-		BOOST_CHECK_EQUAL(mag.second.epicsInterface->isMessagingOn(), true);
+		for (auto& mag : magFac.getAllMagnets())
+		{
+			BOOST_CHECK_EQUAL(mag.second.isMessagingOn(), true);
+			BOOST_CHECK_EQUAL(mag.second.epicsInterface->isMessagingOn(), true);
+		}
 	}
 	magFac.messagesOff();
 	BOOST_CHECK_EQUAL(magFac.isMessagingOn(), false);
 	BOOST_CHECK_EQUAL(magFac.reader.isMessagingOn(), false);
-	for (auto& mag : magFac.getAllMagnets())
+	if (status)
 	{
-		BOOST_CHECK_EQUAL(mag.second.isMessagingOn(), false);
-		BOOST_CHECK_EQUAL(mag.second.epicsInterface->isMessagingOn(), false);
+		for (auto& mag : magFac.getAllMagnets())
+		{
+			BOOST_CHECK_EQUAL(mag.second.isMessagingOn(), false);
+			BOOST_CHECK_EQUAL(mag.second.epicsInterface->isMessagingOn(), false);
+		}
 	}
 	magFac.debugMessagesOn();
 	BOOST_CHECK_EQUAL(magFac.isDebugOn(), true);
 	BOOST_CHECK_EQUAL(magFac.reader.isDebugOn(), true);
-	for (auto& mag : magFac.getAllMagnets())
+	if (status)
 	{
-		BOOST_CHECK_EQUAL(mag.second.isDebugOn(), true);
-		BOOST_CHECK_EQUAL(mag.second.epicsInterface->isDebugOn(), true);
+		for (auto& mag : magFac.getAllMagnets())
+		{
+			BOOST_CHECK_EQUAL(mag.second.isDebugOn(), true);
+			BOOST_CHECK_EQUAL(mag.second.epicsInterface->isDebugOn(), true);
+		}
 	}
 	magFac.debugMessagesOff();
 	BOOST_CHECK_EQUAL(magFac.isDebugOn(), false);
 	BOOST_CHECK_EQUAL(magFac.reader.isDebugOn(), false);
-	for (auto& mag : magFac.getAllMagnets())
+	if (status)
 	{
-		BOOST_CHECK_EQUAL(mag.second.isDebugOn(), false);
-		BOOST_CHECK_EQUAL(mag.second.epicsInterface->isDebugOn(), false);
+		for (auto& mag : magFac.getAllMagnets())
+		{
+			BOOST_CHECK_EQUAL(mag.second.isDebugOn(), false);
+			BOOST_CHECK_EQUAL(mag.second.epicsInterface->isDebugOn(), false);
+		}
 	}
 }
 
