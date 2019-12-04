4cc8caa61423faddc4a3efe99b3367c6964cdb37
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2019-12-04 14:23:51 +0000

    [charge] adding tests (w/o EPICS)

--- EPICSInterface/test/EPICSChargeInterfaceTest.cpp
+++ EPICSInterface/test/EPICSChargeInterfaceTest.cpp
@@ -1,8 +1,8 @@
-#define BOOST_TEST_MODULE EPICSBPMInterfaceTest
+#define BOOST_TEST_MODULE EPICSChargeInterfaceTest
 
 #include <boost/test/unit_test.hpp>
 #include <string>
-#include <EPICSBPMInterface.h>
+#include <EPICSChargeInterface.h>
 // EPICS include
 #ifndef __CINT__
 #include <cadef.h>
@@ -10,65 +10,4 @@
 
 LoggingSystem TEST_LOGGER(true, true);
 
-BOOST_AUTO_TEST_CASE(epics_bpm_interface_put_and_get_value_test)
-{
-	EPICSBPMInterface epicsInterface = EPICSBPMInterface();
-	pvStruct setSA1PV;
-	setSA1PV.fullPVName = "VM-CLA-S02-DIA-BPM-01";
-	setSA1PV.pvRecord = "SA1";
-	epicsInterface.retrieveCHID(setSA1PV);
-	epicsInterface.retrieveCHTYPE(setSA1PV);
-	epicsInterface.retrieveCOUNT(setSA1PV);
-	pvStruct getRA1PV;
-	getRA1PV.fullPVName = "VM-CLA-S02-DIA-BPM-01";
-	getRA1PV.pvRecord = "RA1";
-	epicsInterface.retrieveCHID(getRA1PV);
-	epicsInterface.retrieveCHTYPE(getRA1PV);
-	epicsInterface.retrieveCOUNT(getRA1PV);
-	if (ca_state(setSA1PV.CHID) == cs_conn)
-	{
-		BOOST_CHECK_EQUAL(ca_read_access(setSA1PV.CHID), 1);
-		BOOST_CHECK_EQUAL(ca_write_access(setSA1PV.CHID), 1);
-		srand(time(NULL));
-		long sa1ToSet = rand() % 10;
-		epicsInterface.putValue(setSA1PV, sa1ToSet);
-		long returnValue;
-		ca_get(getRA1PV.CHTYPE, getRA1PV.CHID, &returnValue);
-		ca_pend_io(CA_PEND_IO_TIMEOUT);
-		BOOST_CHECK_EQUAL(returnValue, sa1ToSet);
-	}
-	else
-	{
-		TEST_LOGGER.printMessage("CANNOT CONNECT TO EPICS");
-	}
-}
-
-//BOOST_AUTO_TEST_CASE(epics_magnet_interface_monitor_channel_type_test)
-//{
-//	EPICSMagnetInterface epicsInterface = EPICSMagnetInterface();
-//	pvStruct getSetIPV;
-//	getSetIPV.fullPVName = "VM-CLA-C2V-MAG-VCOR-01";
-//	getSetIPV.pvRecord = "GETSETI";
-//	getSetIPV.monitor = true;
-//	epicsInterface.retrieveCHID(getSetIPV);
-//	epicsInterface.retrieveCHTYPE(getSetIPV);
-//	epicsInterface.retrieveCOUNT(getSetIPV);
-//
-//	pvStruct getRPowerPV;
-//	getRPowerPV.fullPVName = "VM-CLA-C2V-MAG-VCOR-01";
-//	getRPowerPV.pvRecord = "RPOWER";
-//	getRPowerPV.monitor = true;
-//	epicsInterface.retrieveCHID(getRPowerPV);
-//	epicsInterface.retrieveCHTYPE(getRPowerPV);
-//	epicsInterface.retrieveCOUNT(getRPowerPV);
-//
-//	if (ca_state(getSetIPV.CHID) == cs_conn && ca_state(getRPowerPV.CHID) == cs_conn)
-//	{
-//		BOOST_CHECK_EQUAL(getSetIPV.MonitorCHTYPE, DBR_TIME_DOUBLE);
-//		BOOST_CHECK_EQUAL(getRPowerPV.MonitorCHTYPE, DBR_TIME_ENUM);
-//	}
-//	else
-//	{
-//		TEST_LOGGER.printMessage("CANNOT CONNECT TO EPICS");
-//	}
-//}
+// THERE ARE NO CHARGE PVS IN VM, SO SKIP THIS FOR NOW
--- Factories/test/ChargeFactoryTest.cpp
+++ Factories/test/ChargeFactoryTest.cpp
@@ -1,45 +1,15 @@
 #define BOOST_TEST_MODULE BPMFactoryTest
 
 #include <boost/test/unit_test.hpp>
-#include "BPMFactory.h"
+#include "ChargeFactory.h"
 #include <string>
 #include <vector>
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
 
-BOOST_AUTO_TEST_SUITE(BPMFactoryTestSuite)
+BOOST_AUTO_TEST_SUITE(ChargeFactoryTestSuite)
 
-BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_sa1_test)
-{
-	std::string testBPMName = "VM-CLA-S01-DIA-BPM-01";
-	BPMFactory bpmfac = BPMFactory(true);
-	bpmfac.setup("nominal");
-	srand(time(NULL));
-	long sa1ToSet = rand() % 10;
-	bpmfac.setSA1(testBPMName, sa1ToSet);
-	std::this_thread::sleep_for(std::chrono::seconds(10));
-	BOOST_CHECK_EQUAL(bpmfac.getSA1(testBPMName), sa1ToSet);
-}
-
-//BOOST_AUTO_TEST_CASE(magnet_factory_read_i_magnet_test)
-//{
-//	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
-//	MagnetFactory magFac = MagnetFactory(true);
-//	magFac.setup("nominal");
-//	srand(time(NULL));
-//	double currentToSet = rand() % 10 + 1.0;
-//	magFac.setCurrent(testMagnetName, currentToSet);
-//	std::this_thread::sleep_for(std::chrono::seconds(10));
-//	BOOST_CHECK_CLOSE(magFac.getRICurrent(testMagnetName), currentToSet, 10);
-//}
-//
-//BOOST_AUTO_TEST_CASE(magnet_factory_rilk_state_test)
-//{
-//	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
-//	MagnetFactory magFac = MagnetFactory(true);
-//	magFac.setup("nominal");
-//	BOOST_CHECK_EQUAL(magFac.getILKState(testMagnetName), 0);
-//}
+// THERE IS NOTHING IN THE VM FOR CHARGE DIAGNOSTICS, SO SKIP THIS
 
 BOOST_AUTO_TEST_SUITE_END();
