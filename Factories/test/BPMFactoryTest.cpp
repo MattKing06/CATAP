e3ef9d9a2b0315240a62ad50e96a20c144633885
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2019-12-05 16:02:33 +0000

    why doesn't bpmfactorytest work?

--- Factories/test/BPMFactoryTest.cpp
+++ Factories/test/BPMFactoryTest.cpp
@@ -9,7 +9,7 @@
 #include <thread>
 
 BOOST_AUTO_TEST_SUITE(BPMFactoryTestSuite)
-
+LoggingSystem testLogger(true, true);
 BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_sa1_test)
 {
 	std::string testBPMName = "VM-CLA-S01-DIA-BPM-01";
@@ -23,26 +23,10 @@ BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_sa1_test)
 		std::this_thread::sleep_for(std::chrono::seconds(10));
 		BOOST_CHECK_EQUAL(bpmfac.getSA1(testBPMName), sa1ToSet);
 	}
+	else
+	{
+		testLogger.printMessage("CANNOT CONNECT TO EPICS");
+	}
 }
-
-//BOOST_AUTO_TEST_CASE(magnet_factory_read_i_magnet_test)
-//{
-//	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
-//	MagnetFactory magFac = MagnetFactory(true);
-//	magFac.setup("nominal");
-//	srand(time(NULL));
-//	double currentToSet = rand() % 10 + 1.0;
-//	magFac.setCurrent(testMagnetName, currentToSet);
-//	std::this_thread::sleep_for(std::chrono::seconds(10));
-//	BOOST_CHECK_CLOSE(magFac.getRICurrent(testMagnetName), currentToSet, 10);
-//}
-//
-//BOOST_AUTO_TEST_CASE(magnet_factory_rilk_state_test)
-//{
-//	std::string testMagnetName = "VM-CLA-C2V-MAG-HCOR-01";
-//	MagnetFactory magFac = MagnetFactory(true);
-//	magFac.setup("nominal");
-//	BOOST_CHECK_EQUAL(magFac.getILKState(testMagnetName), 0);
-//}
 
 BOOST_AUTO_TEST_SUITE_END();
