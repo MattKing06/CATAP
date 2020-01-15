6915dba721a23cbcdb1afd83bf85cfc98326d1f9
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2020-01-15 15:28:42 +0000

    fixed min max VC++ macro definition in GlobalCOnstants.h, and other typos

--- Utilities/include/GlobalConstants.h
+++ Utilities/include/GlobalConstants.h
@@ -1,14 +1,18 @@
 #ifndef GLOBAL_CONSTANTS_H_
 #define GLOBAL_CONSTANTS_H_
-//#include <limits>
+#include <limits>
 #include <map>
 #include <string>
 
 
 // where i sthe best to put this ??
 // http://forums.codeguru.com/showthread.php?369447-can-t-assign-numeric_limits-lt-double-gt-max()
+#ifdef max
 #undef max
+#endif
+#ifdef min
 #undef min
+#endif
 
 namespace GlobalConstants 
 {
