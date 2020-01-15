1862a48c072b50ddff2c4ab0bec8050bf89598a3
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2020-01-15 13:18:09 +0000

    added ownerName assignment to EPICSInterfaceTest.cpp

--- EPICSInterface/test/EPICSInterfaceTest.cpp
+++ EPICSInterface/test/EPICSInterfaceTest.cpp
@@ -9,10 +9,11 @@
 BOOST_AUTO_TEST_CASE(check_chid_state_from_epics_interface_test)
 {
 	EPICSInterface epicsInterface = EPICSInterface();
-	epicsInterface.messagesOn();
 	pvStruct pv;
 	pv.fullPVName = "VM-CLA-C2V-MAG-VCOR-01";
+	epicsInterface.ownerName = pv.fullPVName;
 	pv.pvRecord = "READI";
+	epicsInterface.messagesOn();
 	epicsInterface.retrieveCHID(pv);
 	if (ca_state(pv.CHID) == cs_conn)
 	{
@@ -28,19 +29,21 @@ BOOST_AUTO_TEST_CASE(check_chid_state_from_epics_interface_test)
 BOOST_AUTO_TEST_CASE(check_put_value_from_epics_interface_test)
 {
 	EPICSInterface epicsInterface = EPICSInterface();
-	epicsInterface.messagesOn();
 	pvStruct pvToSet;
 	pvToSet.fullPVName = "VM-CLA-C2V-MAG-VCOR-01";
+	epicsInterface.ownerName = pvToSet.fullPVName;
 	pvToSet.pvRecord = "SETI";
+	epicsInterface.messagesOn();
 	epicsInterface.retrieveCHID(pvToSet);
 	epicsInterface.retrieveCHTYPE(pvToSet);
 	epicsInterface.retrieveCOUNT(pvToSet);
+
 	if (ca_state(pvToSet.CHID) == cs_conn)
 	{
 		srand(time(NULL));
 		double currentToSet = rand() % 10 + 1.0;
 		double returnedCurrent;
-		epicsInterface.putValue(pvToSet, currentToSet);
+		//epicsInterface.putValue(pvToSet, currentToSet);
 		ca_get(pvToSet.CHTYPE, pvToSet.CHID, &returnedCurrent);
 		ca_pend_io(CA_PEND_IO_TIMEOUT);
 		BOOST_CHECK_CLOSE(currentToSet, returnedCurrent, 1e-10);
