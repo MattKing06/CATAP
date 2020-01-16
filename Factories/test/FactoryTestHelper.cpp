ef7336e0091d0371f4c7cf440b42480b6066afd0
Matt King <matthew.king@stfc.ac.uk>
2020-01-16 12:35:13 +0000

    removed _H_ suffix of FactoryTestHelper define

--- Factories/test/FactoryTestHelper.cpp
+++ Factories/test/FactoryTestHelper.cpp
@@ -1,5 +1,5 @@
-#ifndef FACTORY_TEST_HELPER_H_
-#define FACTORY_TEST_HELPER_H_
+#ifndef FACTORY_TEST_HELPER
+#define FACTORY_TEST_HELPER
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE FactoryTests
 #include <boost/test/unit_test.hpp>
