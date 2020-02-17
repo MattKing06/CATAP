9db45e2b261bdb190feb56aead0494c9c705e439
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2020-02-17 15:17:26 +0000

    [bpm] fixing tactory tests

--- Factories/test/BPMFactoryTest.cpp
+++ Factories/test/BPMFactoryTest.cpp
@@ -7,7 +7,10 @@
 #include <thread>
 
 BOOST_AUTO_TEST_SUITE(BPMFactoryTestSuite)
-
+BOOST_AUTO_TEST_CASE(start_test)
+{
+	BOOST_TEST_MESSAGE("------	RUNNING BPM FACTORY TESTS	------");
+}
 BOOST_AUTO_TEST_CASE(bpm_factory_set_and_check_sa1_test)
 {
 	std::string testBPMName = "VM-CLA-S01-DIA-BPM-01";
