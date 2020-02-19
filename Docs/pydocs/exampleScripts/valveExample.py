40f4fed12239f810c9733e1b1122c4dac8acae39
Matt King <matthew.king@stfc.ac.uk>
2020-02-19 15:39:42 +0000

    added description to ExampleScripts page, edited the example script for valves to use all, multiple, and single getter functions

--- Docs/pydocs/exampleScripts/valveExample.py
+++ Docs/pydocs/exampleScripts/valveExample.py
@@ -2,22 +2,27 @@ from CATAP import *
 
 # create a hardware factory for CLARA hardware
 HF = HardwareFactory(STATE.PHYSICAL)
-# turn on messaging
-HF.messagesOn()
+# turn off messaging
+HF.messagesOff()
+HF.debugMessagesOff()
 # setup the Valve hardware and factory
 HF.setup("Valve", "Nominal")
 # get the Valve factory
 VF = HF.getValveFactory()
 # get all of the valve names into a list
-valv_name_list = VF.getAllValveNames()
+valve_name_list = VF.getAllValveNames()
 # print the name list
-print(valv_name_list)
+print("Valve Name List: ", valve_name_list)
+# create a sub-list of valve names
+valve_name_sub_list = valve_name_list[0:5]
+# get states of valves in the sub-list
+print(VF.getValveStates(valve_name_sub_list))
 # loop over list and print the valve state for each valve by-name
-for valve_name in valv_name_list:
-    print(VF.getValveState(valve_name))
+for valve_name in valve_name_list:
+    print(valve_name, " : ", VF.getValveState(valve_name))
 # get a the "INJ-VAC-VALV-06" valve
 valve_object = VF.getValve("EBT-INJ-VAC-VALV-06")
 # print the name property for the valve
-print(valve_object.name)
+print("Valve Name: ", valve_object.name)
 # print the state property for the valve
-print(valve_object.state)
+print("Valve State: ", valve_object.state)
