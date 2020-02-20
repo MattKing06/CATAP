0fb3bd8a17ab4684afd8777dd7fba68ccc3f400b
Matt King <matthew.king@stfc.ac.uk>
2020-02-20 17:30:40 +0000

    added documentation comments to InvalidFileException class

--- ConfigReader/include/ConfigReaderExceptions.h
+++ ConfigReader/include/ConfigReaderExceptions.h
@@ -1,13 +1,35 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+
+/*! @defgroup exceptions
+ *  @ingroup configreader*/
+/*!@{*/
+/*! @brief Exception thrown when a Master Lattice Configuration File
+		   does not contain a parameter from its 
+*	       associated hardware template file.
+*
+*   This exception should be thrown by the ConfigReader class when it finds a Master Lattice Configuration file
+*   that does not match the specified template of that hardware-type's configuration files.
+*/
 class InvalidFileException : std::runtime_error
 {
 public:
+	/*! Constructor taking associated filename and a single entry that was not present
+	* @param[in] filename : The name of the file that did not match the template.
+	* @param[in] missingEntry : The string of the parameter that the file was missing.*/
 	InvalidFileException(std::string filename, std::string missingEntry);
+    /*! Constructor taking associated filename and multiple entries that were not present
+	* @param[in] filename : The name of the file that did not match the template.
+	* @param[in] missingEntries : A list of the parameters that the file was missing.*/
 	InvalidFileException(std::string filename,  std::vector<std::string> missingEntries);
+	/*! Function to print a formatted message containing filename and missing entries.*/
 	void printError();
+	/*! The name of the file that did not match the template.*/
 	std::string filename;
+	/*! The string of the parameter that the file was missing if we are only missing one parameter.*/
 	std::string missingEntry;
+	/*! The list of the parameters that the file was missing if we are missing multiple parameters.*/
 	std::vector<std::string> missingEntries;
 };
+/*!@}*/
