e92a8e7b6f24e7f9b76b497d33e351315ec51b1e
Matt King <matthew.king@stfc.ac.uk>
2020-02-20 14:02:02 +0000

    created InvalidFileException class and added to ConfigReader CMakeLists

---
+++ ConfigReader/include/ConfigReaderExceptions.h
@@ -0,0 +1,13 @@
+#include <exception>
+#include <string>
+#include <vector>
+class InvalidFileException : std::exception
+{
+public:
+	InvalidFileException(std::string filename, std::string missingEntry);
+	InvalidFileException(std::string filename,  std::vector<std::string> missingEntries);
+	void printError();
+	std::string filename;
+	std::string missingEntry;
+	std::vector<std::string> missingEntries;
+};
