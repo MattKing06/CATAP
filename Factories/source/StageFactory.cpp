d6a92f6da0ee9271aec940635b4e4e4a0de79fb8
Matt King <matthew.king@stfc.ac.uk>
2020-02-21 15:45:32 +0000

    adding functions to cpp and moving python interface to header file.

---
+++ Factories/source/StageFactory.cpp
@@ -0,0 +1,21 @@
+#include <StageFactory.h>
+
+StageFactory::StageFactory()
+{
+}
+
+StageFactory::StageFactory(STATE mode)
+{
+}
+
+StageFactory::StageFactory(const StageFactory& copyFactory)
+{
+}
+
+StageFactory::~StageFactory()
+{
+}
+
+void StageFactory::setup(std::vector<std::string> pvListToMonitor)
+{
+}
