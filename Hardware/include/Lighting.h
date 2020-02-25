34fed9c01e6967418fb1cde8b9bbf122ea665de8
Matt King <matthew.king@stfc.ac.uk>
2020-02-25 11:54:27 +0000

    added source and include files with skeleton code for Lighting object

---
+++ Hardware/include/Lighting.h
@@ -0,0 +1,31 @@
+#ifndef LIGHTING_H_
+#define LIGHTING_H_
+
+#include <Hardware.h>
+#include <EPICSLightingInterface.h>
+#include <GlobalConstants.h>
+#include <GlobalStateEnums.h>
+#include <boost/make_shared.hpp>
+
+class EPICSLightingInterface;
+
+typedef boost::shared_ptr<EPICSLightingInterface> EPICSLightingInterface_sptr;
+
+class Lighting : Hardware
+{
+public:
+	Lighting();
+	Lighting(const std::map<std::string, std::string>& paramMap, STATE mode);
+	Lighting(const Lighting& copyLighting);
+	~Lighting();
+	void setPVStructs();
+	EPICSLightingInterface_sptr epicsInterface;
+	std::map<std::string, std::string> LightingParamMap;
+	void debugMessagesOn();
+	void debugMessagesOff();
+	void messagesOn();
+	void messagesOff();
+};
+
+
+#endif //LIGHTING_H_
---
+++ Utilities/source/LightingPVRecords.cpp
@@ -0,0 +1,1 @@
+#include <LightingPVRecords.h>
