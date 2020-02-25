8ba227c1a43e3d56110cdf0598873be76107197b
Matt King <matthew.king@stfc.ac.uk>
2020-02-25 11:06:46 +0000

    added source and include files and skeleton code for Shutter objects

---
+++ EPICSInterface/test/EPICSShutterInterfaceTest.cpp
@@ -0,0 +1,12 @@
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <EPICSShutterInterface.h>
+// EPICS include
+#ifndef __CINT__
+#include <cadef.h>
+#endif
+
+BOOST_AUTO_TEST_CASE(start_epics_shutter_interface_tests)
+{
+	BOOST_TEST_MESSAGE("------	RUNNING EPICS SHUTTER INTERFACE TESTS	------");
+}
