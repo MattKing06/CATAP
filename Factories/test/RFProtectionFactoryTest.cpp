54d91c9b1cbed94b9318cdbae37ecbf97ca71fab
Matt King <matthew.king@stfc.ac.uk>
2020-02-25 13:16:35 +0000

    added source and include files with skeleton code for RFProtection object

---
+++ Factories/test/RFProtectionFactoryTest.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+#include <RFProtectionFactory.h>
+
+BOOST_AUTO_TEST_SUITE(RFProtectionFactoryTestSuite)
+
+BOOST_AUTO_TEST_CASE(start_rf_protection_factory_tests)
+{
+	BOOST_TEST_MESSAGE("------	RUNNING RF_MODULATOR FACTORY TESTS	------");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
