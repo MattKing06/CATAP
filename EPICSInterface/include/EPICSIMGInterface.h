1a9b624cf0b37a6dc28a7784b5d61b73157fc91c
Matt King <matthew.king@stfc.ac.uk>
2020-02-26 13:21:31 +0000

    added include and source files with skeleton code for IMG object

---
+++ EPICSInterface/include/EPICSIMGInterface.h
@@ -0,0 +1,13 @@
+#ifndef EPICS_IMG_INTERFACE_H_
+#define EPICS_IMG_INTERFACE_H_
+#include <EPICSInterface.h>
+
+class EPICSIMGInterface : EPICSInterface
+{
+public:
+	EPICSIMGInterface();
+	~EPICSIMGInterface();
+	std::string ownerName;
+};
+
+#endif //EPICS_IMG_INTERFACE_H_
---
+++ Hardware/include/IMG.h
@@ -0,0 +1,31 @@
+#ifndef IMG_H_
+#define IMG_H_
+
+#include <Hardware.h>
+#include <EPICSIMGInterface.h>
+#include <GlobalConstants.h>
+#include <GlobalStateEnums.h>
+#include <boost/make_shared.hpp>
+
+class EPICSIMGInterface;
+
+typedef boost::shared_ptr<EPICSIMGInterface> EPICSIMGInterface_sptr;
+
+class IMG : Hardware
+{
+public:
+	IMG();
+	IMG(const std::map<std::string, std::string>& paramMap, STATE mode);
+	IMG(const IMG& copyIMG);
+	~IMG();
+	void setPVStructs();
+	EPICSIMGInterface_sptr epicsInterface;
+	std::map<std::string, std::string> IMGParamMap;
+	void debugMessagesOn();
+	void debugMessagesOff();
+	void messagesOn();
+	void messagesOff();
+};
+
+
+#endif //IMG_H_
