516aeb71962f9b491387d67474c8608a6bdd48de
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2020-03-13 15:10:52 +0000

    [screen] maybe valve factory is the point of failure?

--- Factories/test/ValveFactoryTest.cpp
+++ Factories/test/ValveFactoryTest.cpp
@@ -4,7 +4,7 @@ BOOST_AUTO_TEST_SUITE(ValveFactoryTests)
 
 BOOST_AUTO_TEST_CASE(setting_up_valve_factory_and_get_states)
 {
-	ValveFactory valvFac = ValveFactory(STATE::PHYSICAL);
+	ValveFactory valvFac = ValveFactory(STATE::VIRTUAL);
 	valvFac.setup("nominal");
 	auto stateMap = valvFac.getAllValveStates();
 	for (auto&& item : stateMap)
