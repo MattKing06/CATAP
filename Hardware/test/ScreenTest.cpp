33a958634e5fca215f6ca671a87f9272bddad4e7
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2020-03-13 14:31:27 +0000

    [screen] commenting out configreadertest functions as this was causing failure

--- Hardware/test/ScreenTest.cpp
+++ Hardware/test/ScreenTest.cpp
@@ -11,4 +11,12 @@
 
 BOOST_AUTO_TEST_SUITE(ScreenTestSuite)
 
+BOOST_AUTO_TEST_CASE(creating_empty_screen_test)
+{
+	Screen scr = Screen();
+	BOOST_CHECK_EQUAL(scr.getHardwareName().c_str(), "");
+	auto pvStructMap = scr.getPVStructs();
+	BOOST_CHECK_EQUAL(pvStructMap.empty(), true);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
