526d58202997030cbad37b731dbbb431fe0b4f33
King <matthew.king@stfc.ac.uk>
2020-03-24 17:34:10 +0000

    changed top-level init file to add relative paths to sys path so that the rest of the sub-modules are visible to python

--- PythonInterface/init/__init__.py
+++ PythonInterface/init/__init__.py
@@ -1,9 +1,6 @@
-from CATAP import *
 import sys
 import os
 
-directory_list = next(os.walk('./CATAP'))[1]
-directory_list.remove('__pycache__')
-print(directory_list)
-for directory in directory_list:
-    sys.path.append(os.path.join(os.path.dirname(__file__), directory))
+for path, directories, files in os.walk('./'):
+    if "__pycache__" not in path:
+        sys.path.append(path)
