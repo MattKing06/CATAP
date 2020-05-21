8b245c8a1c4f9a4a496675cf62d52a8ce0ed7dbc
Scott <duncan.scott@stfc.ac.uk>
2020-05-21 12:28:43 +0100

    adeded dummy test files and removed llrf.h llrf.cpp form wrong folder

---
+++ EPICSInterface/test/EPICSLLRFInterfaceTest.cpp
@@ -0,0 +1,12 @@
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <EPICSLLRFInterface.h>
+// EPICS include
+#ifndef __CINT__
+#include <cadef.h>
+#endif
+
+BOOST_AUTO_TEST_CASE(start_epics_llrf_interface_tests)
+{
+	BOOST_TEST_MESSAGE("------	RUNNING EPICS LLRF INTERFACE TESTS	------");
+}
