0edb3402abcc62ffcf66242b4afcf31634d35fc2
King <matthew.king@stfc.ac.uk>
2020-07-22 15:39:05 +0100

    filled out Putter class with setupChannels and put/put_Py functions

--- EPICSTools/include/Putter.h
+++ EPICSTools/include/Putter.h
@@ -1,10 +1,46 @@
 #ifndef PUTTER_H
 #define PUTTER_H
 
+#include <EPICSInterface.h>
+#include <GlobalStateEnums.h>
+#include <GlobalConstants.h>
+#include <LoggingSystem.h>
+#include <PV.h>
+#include <string>
+#include <vector>
+#include <boost/make_shared.hpp>
+#include <boost/python.hpp>
+
+class EPICSInterface;
+typedef boost::shared_ptr<EPICSInterface> EPICSInterface_sptr;
+
 class Putter
 {
 public:
 	Putter();
+	Putter(const std::string& pv);
+	Putter(STATE mode, const std::string& pv);
+	Putter(const Putter& copyPutter);
+	void setupChannels();
+
+	//With PUT function, will have to check the EPICS CHTYPE 
+	//and use appropriate boost::python::Extract template to put value
+	//from a python object..
+
+	void put_Py(boost::python::object value);
+	template <typename T>
+	void put(T value);
+	EPICSInterface_sptr epicsInterface;
+	STATE mode;
+	LoggingSystem messenger;
+	pvStruct pv;
+	std::string pvToPut;
 };
 
-#endif PUTTER_H
+template<typename T>
+inline void Putter::put(T value)
+{
+	epicsInterface->putValue<T>(pv, value);
+}
+
+#endif //PUTTER_H
--- EPICSTools/source/Putter.cpp
+++ EPICSTools/source/Putter.cpp
@@ -4,3 +4,88 @@ Putter::Putter()
 {
 
 }
+
+Putter::Putter(const std::string& pv) :
+	epicsInterface(boost::make_shared<EPICSInterface>()),
+	pvToPut(pv),
+	mode(STATE::UNKNOWN),
+	messenger(LoggingSystem(false, true))
+{
+	setupChannels();
+}
+
+Putter::Putter(STATE mode, const std::string& pv) :
+	epicsInterface(boost::make_shared<EPICSInterface>()),
+	pvToPut(pv),
+	mode(mode),
+	messenger(LoggingSystem(false, true))
+{
+	setupChannels();
+}
+
+Putter::Putter(const Putter& copyPutter) : 
+	epicsInterface(copyPutter.epicsInterface),
+	pvToPut(copyPutter.pvToPut),
+	mode(copyPutter.mode),
+	messenger(copyPutter.messenger),
+	pv(copyPutter.pv)
+{
+	setupChannels();
+}
+
+void Putter::setupChannels()
+{
+	pv = pvStruct();
+	pv.fullPVName = pvToPut;
+	pv.monitor = false;
+	epicsInterface->retrieveCHID(pv);
+	EPICSInterface::sendToEPICS();
+	epicsInterface->retrieveCHTYPE(pv);
+	std::cout << "CH TYPE: " << pv.CHTYPE << std::endl;
+	epicsInterface->retrieveCOUNT(pv);
+	pv.MASK = DBE_VALUE;
+	EPICSInterface::sendToEPICS();
+}
+
+void Putter::put_Py(boost::python::object pyValue)
+{
+	switch (pv.CHTYPE) 
+	{
+		case(DBR_DOUBLE):
+		{
+			double d_value = boost::python::extract<double>(pyValue);
+			epicsInterface->putValue<double>(pv, d_value);
+			break;
+		}
+		case(DBR_ENUM):
+		{
+			unsigned short us_value = boost::python::extract<unsigned short>(pyValue);
+			epicsInterface->putValue<unsigned short>(pv, us_value);
+			break;
+		}
+		case(DBR_INT):
+		{
+			int i_value = boost::python::extract<int>(pyValue);
+			epicsInterface->putValue<int>(pv, i_value);
+			break;
+		}
+		case(DBR_LONG):
+		{
+			long l_value = boost::python::extract<long>(pyValue);
+			epicsInterface->putValue<long>(pv, l_value);
+			break;
+		}
+		case(DBR_FLOAT):
+		{
+			float f_value = boost::python::extract<float>(pyValue);
+			epicsInterface->putValue<float>(pv, f_value);
+			break;
+		}
+		case(DBR_CHAR):
+		{
+			char ch_value = boost::python::extract<char>(pyValue);
+			epicsInterface->putValue<char>(pv, ch_value);
+			break;
+		}
+	}
+}
