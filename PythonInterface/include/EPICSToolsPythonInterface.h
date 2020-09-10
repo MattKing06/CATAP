7e4b26d19599b3d77a80ee7a052d0a9c2ee351d4
King <matthew.king@stfc.ac.uk>
2020-09-10 16:44:37 +0100

    started python documentation

--- PythonInterface/include/EPICSToolsPythonInterface.h
+++ PythonInterface/include/EPICSToolsPythonInterface.h
@@ -15,20 +15,18 @@ namespace BOOST_PYTHON_EPICS_TOOLS_INCLUDE
 		if (is_registered) return;
 		boost::python::class_<Listener, boost::noncopyable>("Monitor", boost::python::no_init)
 			.add_property("PV", &Listener::pvToMonitor)
-			.def("getValue", &Listener::getValue_Py)
-			.def("getArray", &Listener::getArray_Py)
-			.def("setBufferSize", &Listener::setBufferSize)
-			.def("setArrayBufferSize", &Listener::setArrayBufferSize)
-			.def("getBuffer", &Listener::getBuffer_Py)
-			.def("getBufferAverage", &Listener::getBufferAverage)
-			.def("getArrayBuffer", &Listener::getArrayBuffer_Py)
-			.def("getArrayBufferAverage", &Listener::getArrayBufferAverageArray_Py);
+			.def("getValue", &Listener::getValue_Py, "get current value of the monitor")
+			.def("getArray", &Listener::getArray_Py, "get current array of the monitor")
+			.def("setBufferSize", &Listener::setBufferSize, "set the size of the buffer (default size is 10)")
+			.def("setArrayBufferSize", &Listener::setArrayBufferSize, "set the size of the array buffer (default size is 10)")
+			.def("getBuffer", &Listener::getBuffer_Py, "get the buffer of past values from EPICS")
+			.def("getBufferAverage", &Listener::getBufferAverage, "get the mean of the values in buffer")
+			.def("getArrayBuffer", &Listener::getArrayBuffer_Py, "get the buffer of past arrays from EPICS")
+			.def("getArrayBufferAverage", &Listener::getArrayBufferAverageArray_Py, "get the mean array of arrays in buffer");
 
 	}
 	void expose_epics_tools_object() 
 	{
-		bool is_registered = (0 != boost::python::converter::registry::query(boost::python::type_id <EPICSTools>())->to_python_target_type());
-		if (is_registered) return;
 		void(EPICSTools:: * monitor_single)(const std::string&) = &EPICSTools::monitor;
 		boost::python::object(EPICSTools:: * get_single)(const std::string&) = &EPICSTools::get_Py;
 		boost::python::dict(EPICSTools:: * get_multiple)(boost::python::list) = &EPICSTools::get_Py;
@@ -40,10 +38,12 @@ namespace BOOST_PYTHON_EPICS_TOOLS_INCLUDE
 		boost::python::dict(EPICSTools:: * getBufferAverage_multiple)(boost::python::list) = &EPICSTools::getBufferAverage_Py;
 		double(EPICSTools:: * getBufferStdDeviation_single)(const std::string&) = &EPICSTools::getBufferStdDeviation;
 		boost::python::dict(EPICSTools:: * getBufferStdDeviation_multiple)(boost::python::list) = &EPICSTools::getBufferStdDeviation_Py;
+		bool is_registered = (0 != boost::python::converter::registry::query(boost::python::type_id <EPICSTools>())->to_python_target_type());
+		if (is_registered) return;
 		boost::python::class_<EPICSTools, boost::noncopyable>("EPICSTools", boost::python::no_init)
 			.def(boost::python::init<>())
 			.def(boost::python::init<STATE>())
-			.def("monitor", &EPICSTools::monitor_Py)
+			.def("monitor", &EPICSTools::monitor_Py, "begin monitoring")
 			.def("monitor", monitor_single)
 			.def("stopMonitoring", &EPICSTools::stopMonitoring)
 			.def("restartMonitoring", &EPICSTools::restartMonitoring)
