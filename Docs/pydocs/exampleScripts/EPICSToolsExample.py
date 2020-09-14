42eb20b669886d3b12868c27f6aedfb832c85011
King <matthew.king@stfc.ac.uk>
2020-09-14 10:42:09 +0100

    updated example script

--- Docs/pydocs/exampleScripts/EPICSToolsExample.py
+++ Docs/pydocs/exampleScripts/EPICSToolsExample.py
@@ -1,5 +1,6 @@
 from CATAP.EPICSTools import *
-
+import random
+import time
 # Creating an EPICSTools object without the STATE argument
 # will force CATAP to use the Virtual EPICS instance.
 # i.e. ET = EPICSTools()
@@ -13,16 +14,13 @@ ET.monitor(readi_pv)
 
 # To access the monitor object, it can be accessed via EPICSTools:
 readi_monitor = ET.getMonitor(readi_pv)
-# From this object, the value, buffer, buffer average, 
-# and buffer standard deviation can be accessed:
 print("Current READI Value: ", readi_monitor.getValue())
-print("Current READI Buffer: ", readi_monitor.getBuffer())
-print("READI Buffer Average: ", readi_monitor.getBufferAverage())
-print("READI Buffer Standard Deviation: ", readi_monitor.getBufferStdDev())
+
 
 # Putting to a PV can only be done through the EPICSTools object
 seti_pv = "CLA-C2V-MAG-HCOR-01:SETI"
-ET.put(seti_pv, 10.0)
+set_current = int(random.random() * 10)
+ET.put(seti_pv, set_current)
 
 # Getting a PV value can only be done through the EPICSTools object
 getseti_pv = "CLA-C2V-MAG-HCOR-01:GETSETI"
@@ -31,6 +29,13 @@ ET.get(getseti_pv)
 # Example of using monitor, put, and get:
 spower_pv = "CLA-C2V-MAG-HCOR-01:SPOWER"
 ET.put(spower_pv, 1)
-while(readi_monitor.getValue() != ET.get(getseti)):
-    print("READI VALUE: ", readi_monitor.getValue()
+while(readi_monitor.getValue() != ET.get(getseti_pv)):
+    print("READI VALUE: ", readi_monitor.getValue())
+    time.sleep(0.1)
 print("READI VALUE: ", readi_monitor.getValue(), " SETI VALUE: ", ET.get(getseti_pv))
+
+# From the monitor object, buffer, buffer average, 
+# and buffer standard deviation can be accessed:
+print("Current READI Buffer: ", readi_monitor.getBuffer())
+print("READI Buffer Average: ", readi_monitor.getBufferAverage())
+print("READI Buffer Standard Deviation: ", readi_monitor.getBufferStdDev())
