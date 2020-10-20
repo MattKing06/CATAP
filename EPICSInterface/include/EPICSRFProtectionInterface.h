1e22cc1175323a14adbe763fa9582d5b20f40dac
King <matthew.king@stfc.ac.uk>
2020-10-20 14:59:43 +0100

    filled in stubs for EPICSRFProtectionInterface

--- EPICSInterface/include/EPICSRFProtectionInterface.h
+++ EPICSInterface/include/EPICSRFProtectionInterface.h
@@ -1,13 +1,24 @@
 #ifndef EPICS_RF_PROTECTION_INTERFACE_H_
 #define EPICS_RF_PROTECTION_INTERFACE_H_
 #include <EPICSInterface.h>
-
+#ifndef RF_PROTECTION_H_
+#include <RFProtection.h>
+#endif // RF_PROTECTION_H_
 class EPICSRFProtectionInterface : public EPICSInterface
 {
 public:
 	EPICSRFProtectionInterface();
+	EPICSRFProtectionInterface(const EPICSRFProtectionInterface& copyRFProtection);
 	~EPICSRFProtectionInterface();
+	void retrieveUpdateFunctionForRecord(const pvStruct& pv);
+	static void updateProtectionStatus(const struct event_handler_args args);
+	static void updateProtectionCmi(const struct event_handler_args args);
+	bool setProtectionOn(const pvStruct& pv);
+	bool setProtectionOff(const pvStruct& pv);
+	bool resetProtection(const pvStruct& pv);
 	std::string ownerName;
+	// We need to a STATIC messenger so that the static call back functions can use it to print messages 
+	static LoggingSystem messenger;
 };
 
 #endif //EPICS_RF_PROTECTION_INTERFACE_H_
--- EPICSInterface/source/EPICSRFProtectionInterface.cpp
+++ EPICSInterface/source/EPICSRFProtectionInterface.cpp
@@ -1,9 +1,58 @@
 #include <EPICSRFProtectionInterface.h>
+#include <RFProtectionPVRecords.h>
 
-EPICSRFProtectionInterface::EPICSRFProtectionInterface()
+LoggingSystem EPICSRFProtectionInterface::messenger;
+
+EPICSRFProtectionInterface::EPICSRFProtectionInterface() : EPICSInterface()
 {
+	messenger = LoggingSystem(true, true);
+}
+
+EPICSRFProtectionInterface::EPICSRFProtectionInterface(const EPICSRFProtectionInterface& copyInterface)
+{
+	messenger.printDebugMessage("EPICSRFProtectionInterface Copy Constructor Called");
 }
 
 EPICSRFProtectionInterface::~EPICSRFProtectionInterface()
 {
+	messenger.printDebugMessage("EPICSRFProtectionInterface Destructor Called");
+}
+
+void EPICSRFProtectionInterface::retrieveUpdateFunctionForRecord(const pvStruct& pv)
+{
+	if (pv.pvRecord == RFProtectionRecords::Sta)
+	{
+		pv.updateFunction = updateProtectionStatus;
+	}
+	if (pv.pvRecord == RFProtectionRecords::Cmi)
+	{
+		pv.updateFunction = updateProtectionCmi;
+	}
+}
+
+void EPICSRFProtectionInterface::updateProtectionStatus(const struct event_handler_args args)
+{
+	RFProtection* protection = getHardwareFromArgs<RFProtection>(args);
+	messenger.printMessage(protection->getHardwareName());
+}
+
+void EPICSRFProtectionInterface::updateProtectionCmi(const struct event_handler_args args)
+{
+	RFProtection* protection = getHardwareFromArgs<RFProtection>(args);
+	messenger.printMessage(protection->getHardwareName());
+}
+
+bool EPICSRFProtectionInterface::setProtectionOn(const pvStruct& pv)
+{
+	return false;
+}
+
+bool EPICSRFProtectionInterface::setProtectionOff(const pvStruct& pv)
+{
+	return false;
+}
+
+bool EPICSRFProtectionInterface::resetProtection(const pvStruct& pv)
+{
+	return false;
 }
