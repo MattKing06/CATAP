4e6042f6166234a2f8f3bbff00623b801f0bc93b
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2021-02-26 15:53:45 +0000

    added rfheartbeat test

--- Factories/test/RFHeartbeatFactoryTest.cpp
+++ Factories/test/RFHeartbeatFactoryTest.cpp
@@ -10,8 +10,6 @@ BOOST_AUTO_TEST_CASE(start_rf_heartbeat_factory_tests)
 
 BOOST_AUTO_TEST_CASE(rf_heartbeat_factory_instantiation_test)
 {
-	std::string testMagnetName = "CLA-C2V-MAG-HCOR-01";
-	//std::string testMagnetName = "EBT-INJ-MAG-QUAD-12";
 	RFHeartbeatFactory fac = RFHeartbeatFactory(STATE::PHYSICAL);
 	fac.messagesOn();
 	fac.debugMessagesOn();
