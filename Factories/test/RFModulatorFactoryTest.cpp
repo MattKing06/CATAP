15c12e8a9c95a132c22b2a284e487ce7dc99636e
djs56 <duncan.scott@stfc.ac.uk>
2021-04-13 16:36:54 +0100

    removed physical test

--- Factories/test/RFModulatorFactoryTest.cpp
+++ Factories/test/RFModulatorFactoryTest.cpp
@@ -11,25 +11,16 @@ BOOST_AUTO_TEST_CASE(start_rf_modulator_factory_tests)
 
 BOOST_AUTO_TEST_CASE(setting_up_llrf_factory_print_names)
 {
-	RFModulatorFactory fac = RFModulatorFactory(STATE::VIRTUAL);
+	RFModulatorFactory fac = RFModulatorFactory(STATE::PHYSICAL);
 	fac.debugMessagesOff();
 	fac.messagesOff();
 	fac.setup("nominal");
 	auto names = fac.getAllRFModulatorNames();
-	for (auto&& item : fac.getAllRFModulatorNames())
+	for (auto&& item : names)
 	{
 		fac.messenger.printDebugMessage("RF Modulator Object name : " + item);
 	}
 }
 
-BOOST_AUTO_TEST_CASE(checking_hvps_voltage_hi_alarm_read)
-{
-	RFModulatorFactory fac = RFModulatorFactory(STATE::PHYSICAL);
-	fac.debugMessagesOff();
-	fac.messagesOff();
-	fac.setup("nominal");
-	RFModulator l01Mod = fac.getModulator("CLA-L01-HRF-MOD-01");
-	
-}
 
 BOOST_AUTO_TEST_SUITE_END()
