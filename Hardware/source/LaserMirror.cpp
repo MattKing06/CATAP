d815a0d1e346b6bf16bdba2135a0f954f48293ca
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2021-07-01 15:23:50 +0100

    using ABS not FABS or LABS or DABS (they are c-functions, this is c++)

--- Hardware/source/LaserMirror.cpp
+++ Hardware/source/LaserMirror.cpp
@@ -86,7 +86,7 @@ void LaserMirror::messagesOff()
 
 void LaserMirror::setHStep(const double& value)
 {
-	if (std::fabs(value) <= maximumStepSize)
+	if (std::abs(value) <= maximumStepSize)
 	{
 		hStep = value;
 	}
@@ -105,7 +105,7 @@ void LaserMirror::setHStep(const double& value)
 
 void LaserMirror::setVStep(const double& value)
 {
-	if (std::fabs(value) <= maximumStepSize)
+	if (std::abs(value) <= maximumStepSize)
 	{
 		vStep = value;
 	}
