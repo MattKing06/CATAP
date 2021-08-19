35b2fd8d498a7e9af7142df0b65fd25bbf4a1081
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2021-08-19 16:14:29 +0100

    update functions added (MABSS, RPOSS) and set function for MABSS

--- EPICSInterface/include/EPICSStageInterface.h
+++ EPICSInterface/include/EPICSStageInterface.h
@@ -1,12 +1,19 @@
 #ifndef EPICS_STAGE_INTERFACE_H_
 #define EPICS_STAGE_INTERFACE_H_
 #include <EPICSInterface.h>
+#include <StagePVRecords.h>
+#include <Stage.h>
 
 class EPICSStageInterface : public EPICSInterface
 {
 public:
 	EPICSStageInterface();
 	~EPICSStageInterface();
+	void retrieveUpdateFunctionForRecord(pvStruct& pv);
+	static void updateMABSS(const struct event_handler_args);
+	static void updateRPOSS(const struct event_handler_args);
+	bool setNewPosition(const pvStruct& pv, const double& newPosition);
+	static LoggingSystem static_messenger;
 	std::string ownerName;
 };
 
