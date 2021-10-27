3b504eff0bf38876a33fbfa619147e8c91a3229c
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2021-10-27 16:19:00 +0100

    now able to construct datetime from two strings

--- PythonInterface/extensions/EPICSArchiverExtension.py
+++ PythonInterface/extensions/EPICSArchiverExtension.py
@@ -6,10 +6,10 @@ class EPICSArchiver(object):
 
     def __init__(self):
         self.archiver_root = "http://claraserv2.dl.ac.uk:17668/retrieval/data/"
-    def printMessage(self, message):
+    def _printMessage(self, message):
         print(message)
     
-    def formURLStr(self, pv, dateFrom, timeFrom, dateTo, timeTo):
+    def _formURLStr(self, pv, dateFrom, timeFrom, dateTo, timeTo):
         pv = pv.replace(':', '%3A')
         urlString = ("getData.json?pv=" + str(pv) +
                      "&from=" + str(dateFrom.year) +
@@ -24,8 +24,17 @@ class EPICSArchiver(object):
                      str(timeTo.minute) + '%3A00.000Z')
         return urlString
     
+    def _makeDateFromStr(self, date_str):
+        return datetime.strptime(date_str, "%d-%m-%Y")
+    
+    def _makeTimeFromStr(self, time_str):
+        return datetime.strptime(time_str, "%H:%M:%S").time()
+    
+    def _makeDateTimeFromStr(self, date_str, time_str):
+        return datetime.combine(self._makeDateFromStr(date_str), self._makeTimeFromStr(time_str))
+    
     def getData(self, pv, dateFrom, timeFrom, dateTo, timeTo):
-        archiver_link = self.archiver_root + self.formURLStr(pv, dateFrom, timeFrom, dateTo, timeTo)
+        archiver_link = self.archiver_root + self._formURLStr(pv, dateFrom, timeFrom, dateTo, timeTo)
         print("REQUESTING DATA FROM: ", archiver_link)
         req = urllib2.urlopen(archiver_link)
         data = json.load(req)
