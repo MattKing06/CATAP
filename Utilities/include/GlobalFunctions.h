81f060b6bf60dd399a96bc3a233d2816e5e7d95e
CLARA Control Room <ALICEMCR@stfc.ac.uk>
2021-11-06 15:17:18 +0000

    fixing both boolToString and stdStringFromCStr function

--- Utilities/include/GlobalFunctions.h
+++ Utilities/include/GlobalFunctions.h
@@ -72,7 +72,7 @@ namespace GlobalFunctions {
 		return true;
 	}
 
-	inline const std::string boolToString(const bool& b) const { return b ? "true" : "false"; }
+	inline const std::string boolToString(const bool& b) { return b ? "true" : "false"; }
 
 	template<class T>
 	bool entryExists(const std::vector<T>& v, const T& item)
@@ -190,7 +190,7 @@ namespace GlobalFunctions {
 	/* THIS CAUSES Virtual Circuit Disconnect due to not being static...*/
 	inline std::string& cStringToStdString(const char* c_string)
 	{
-		std::string newStr = std::string(c_string);
+		std::string newStr = c_string;
 		//remove null-termination char from c-string
 		newStr.erase(newStr.find('\0'));
 		return newStr;
